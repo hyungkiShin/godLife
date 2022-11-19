a679de786eaf899a81dcafc1fb5adf8915af91c4
hyungkiShin <tkaqkeldk@nate.com>
2022-11-19 23:26:26 +0900

    [feat] calculateGrade 리펙토링 완

--- src/main/java/ooppractice/gradeCalculator/Courses.java
+++ src/main/java/ooppractice/gradeCalculator/Courses.java
@@ -10,15 +10,21 @@ public class Courses {
         this.courses = courses;
     }
 
-    public double multiplyCreditAndCourseGrade() {
-        double multipliedCreditAndCourceGrade = 0;
-        for(Cource course: courses) {
-            // as-is -> getter 가 만능은 아니다 multipliedCreditAndCourceGrade += course.getCredit() * course.getGradeToNumber();
-            // to-be -> 응집도를 높히기 위하여 (여러군대에서 쓰이는 중에 요구사항이 변하면 답도 없다.) 객체에게 작업을 위윔 (메세지를 전달) 해서 결과를 받아온다.
-            multipliedCreditAndCourceGrade += course.multiplyCreditAndCourseGrade();
-        }
+//    public double multiplyCreditAndCourseGrade() {
+//        double multipliedCreditAndCourceGrade = 0;
+//        for(Cource course: courses) {
+//            // as-is -> getter 가 만능은 아니다 multipliedCreditAndCourceGrade += course.getCredit() * course.getGradeToNumber();
+//            // to-be -> 응집도를 높히기 위하여 (여러군대에서 쓰이는 중에 요구사항이 변하면 답도 없다.) 객체에게 작업을 위윔 (메세지를 전달) 해서 결과를 받아온다.
+//            multipliedCreditAndCourceGrade += course.multiplyCreditAndCourseGrade();
+//        }
+//
+//        return multipliedCreditAndCourceGrade;
+//    }
 
-        return multipliedCreditAndCourceGrade;
+    public double multiplyCreditAndCourseGrade() {
+        return courses.stream()
+                .mapToDouble(Cource::multiplyCreditAndCourseGrade)
+                .sum();
     }
 
     public int calculateTotalCompletedCredit() {
